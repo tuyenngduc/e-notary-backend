621d1919f25f91959e456a3fa5e0b220bd6584f8
tuyenngduc <tuyenngduc12@gmail.com>
2026-01-23 17:53:45 +0700

    init

---
+++ src/test/java/com/actvn/enotary/ENotarySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actvn.enotary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ENotarySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
