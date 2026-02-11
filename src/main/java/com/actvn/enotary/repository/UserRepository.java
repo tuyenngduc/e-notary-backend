bb1985905806e98b9b128f9e13f5b863904c8bdc
tuyenngduc <tuyenngduc12@gmail.com>
2026-02-11 20:52:26 +0700

    update

---
+++ src/main/java/com/actvn/enotary/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.actvn.enotary.repository;
+
+import com.actvn.enotary.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByPhoneNumber(String phoneNumber);
+    boolean existsByEmail(String email);
+}
