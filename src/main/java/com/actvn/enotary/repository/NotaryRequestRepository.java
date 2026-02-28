5a458fdc856a89c5bf167047819dacd1d43bfd3e
tuyenngduc <tuyenngduc12@gmail.com>
2026-03-01 05:13:16 +0700

    done list-requests

--- src/main/java/com/actvn/enotary/repository/NotaryRequestRepository.java
+++ src/main/java/com/actvn/enotary/repository/NotaryRequestRepository.java
@@ -1,6 +1,9 @@
 package com.actvn.enotary.repository;
 
 import com.actvn.enotary.entity.NotaryRequest;
+import com.actvn.enotary.enums.RequestStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +13,8 @@ import java.util.UUID;
 @Repository
 public interface NotaryRequestRepository extends JpaRepository<NotaryRequest, UUID> {
     List<NotaryRequest> findByClientUserId(UUID userId);
+
+    Page<NotaryRequest> findByStatus(RequestStatus status, Pageable pageable);
+
+    Page<NotaryRequest> findByNotaryUserIdAndStatus(UUID notaryUserId, RequestStatus status, Pageable pageable);
 }
